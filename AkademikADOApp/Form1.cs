1a70b464d85b32279255ecd35f8b764f39024004
miftahuljannahhh <mj5416386@gmail.com>
2026-03-10 13:43:45 +0700

    Membuat Variabel Connection String

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -9,7 +9,12 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
-
+namespace AkademikADOApp
+{
+    public partial class Form1 : Form
+    {
+        string connString =
+       "Data Source=MIFTAHULJANNAH\\MIFTAHJW;Initial Catalog=DBAkademikADO;Integrated Security=True";
 
 
 
