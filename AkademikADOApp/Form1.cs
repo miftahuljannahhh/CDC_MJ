3721dd0c8bc608dac4faf5a463fe59e818438363
miftahuljannahhh <mj5416386@gmail.com>
2026-03-10 13:47:59 +0700

    Membuat Objek SqlConnection

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -9,12 +9,15 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
-namespace AkademikADOApp
+namespace CDC_MJ
 {
     public partial class Form1 : Form
     {
         string connString =
        "Data Source=MIFTAHULJANNAH\\MIFTAHJW;Initial Catalog=DBAkademikADO;Integrated Security=True";
+        SqlConnection conn;
+
+
 
 
 
